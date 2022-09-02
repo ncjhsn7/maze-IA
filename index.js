8bf7315ef2b49fab65be46949fac23af43a23dcc
Nicolas Pietro <52969249+ncjhsn7@users.noreply.github.com>
2022-09-02 02:25:21 -0300

    sla

---
+++ index.js
@@ -0,0 +1,135 @@
+const NxN = 10;
+let grid = new Array(NxN);
+
+function Spot(x,y){
+    this.x = x;
+    this.y = y;
+    this.f = 0;
+    this.g = 0;
+    this.h = 0;
+    this.neighbors = [];
+    this.previous = null;
+    this.wall = false;
+
+    // if(Math.random(1) < 0.2){
+    //     this.wall = true;
+    // }
+
+    this.addNeighbors = (grid) => {
+        if(this.x < NxN - 1){
+            this.neighbors.push(grid[this.x + 1][this.y]);
+        }
+
+        if(this.x > 0){
+            this.neighbors.push(grid[this.x - 1][this.y]);
+        }
+
+        if(this.y < NxN - 1){
+            this.neighbors.push(grid[this.x][this.y + 1]);
+        }
+
+        if(this.y > 0){
+            this.neighbors.push(grid[this.x][this.y - 1]);
+        }
+    }
+}
+
+function remove(arr, element){
+    for (let i = arr.length-1; i >= 0; i--) {
+        if(arr[i] == element){
+            arr.splice(i,1);
+        }        
+    }
+}
+
+function heuristic(a,b){
+    return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+    // return Math.sqrt((a.x - b.x)**2 + (a.y - b.y)**2)
+}
+
+function printGrid(grid, end, current){
+    for(let i = 0; i < NxN; i++){
+        for(let j = 0; j < NxN; j++){
+            let cell = !grid[i][j].wall ? ' - ' : ' x ';
+            if(grid[i][j] == end){
+                cell = '!';
+            }
+            if(grid[i][j] == current){
+                cell = 'N';
+            }
+            process.stdout.write(cell);
+        }
+        console.log();
+    }
+}
+
+for(let i = 0; i < NxN; i++){
+    grid[i] = new Array(NxN);
+}
+
+for(let i = 0; i < NxN; i++){
+    for(let j = 0; j < NxN; j++){
+        grid[i][j] = new Spot(i,j);
+    }
+}
+
+for(let i = 0; i < NxN; i++){
+    for(let j = 0; j < NxN; j++){
+        grid[i][j].addNeighbors(grid);
+    }
+}
+
+let start = grid[0][0];
+let end = grid[9][9]; //verificar se existe comida e recalcular 'end'
+let openSet = [];
+let closedSet = [];
+let path = [];
+openSet.push(start);
+printGrid(grid,end, start);
+while(openSet.length > 0){
+    let best = 0;
+    for (let i = 0; i < openSet.length; i++) {
+        if(openSet[i].f < openSet[best].f){
+            best = i;
+        }
+    }
+
+    let current = openSet[best];
+    
+    if(current == end){
+        path = [];
+        let temp = current;
+        path.push(temp);
+        while(temp.previous){
+            path.push(temp.previous);
+            temp = temp.previous;
+        }
+        path.forEach(element =>{
+            console.log(element.x, element.y);
+            //process.stdout.write(element.x);
+        })
+    }
+    
+    remove(openSet, current);    
+    closedSet.push(current);
+
+    var neighbors = current.neighbors;
+
+    for (let i = 0; i < neighbors.length; i++) {
+        let neighbor = neighbors[i];
+
+        if(!closedSet.includes(neighbor)){
+            let tempG = current.g + 1;
+
+            if(!openSet.includes(neighbor) && tempG > neighbor.g){
+                neighbor.g = tempG;
+                openSet.push(neighbor);
+            }
+
+            neighbor.h = heuristic(neighbor, end);
+            neighbor.f = neighbor.g + neighbor.h;
+            neighbor.previous = current;
+        }
+
+    }
+}
