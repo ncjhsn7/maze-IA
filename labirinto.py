8a764d02e5588980bc2e60b8fc207cf87f71d50b
Lucas Ramos <lucas.ramos@datacom.ind.br>
2022-09-25 21:58:47 -0300

    main.py

--- labirinto.py
+++ labirinto.py
@@ -1,9 +1,9 @@
 '''Classe que representa um labiirinto'''
 
-class labiirinto:
+class Labirinto:
     def __init__(self):
         self.labi = []
-        arquivo = open('labiirinto.txt', 'r')
+        arquivo = open('labirinto.txt', 'r')
         i = 1
         for linha in arquivo:
             pos = linha.split(',')
@@ -12,8 +12,12 @@ class labiirinto:
             i += 1
         self.x = 0
         self.y = 0
+        self.comidas = len(self.labi[0]) // 2
 
-    def print_labiirinto(self):
+    def get_comidas(self):
+        return self.comidas
+
+    def print_labirinto(self):
         for i in range(0,10):
             print(self.labi[i])
         print('--------------------------------------------------')
@@ -27,27 +31,44 @@ class labiirinto:
             5: up left
             6: down right
             7: down left '''
-        self.labi[self.y][self.x] = ' '
         if movimento == 0:
-            self.y = self.y - 1
+            if (self.y-1 >= 0) and (self.labi[self.y-1][self.x] != '1'):
+                self.labi[self.y][self.x] = ' '
+                self.y = self.y - 1
         elif movimento == 1:
-            self.y = self.y + 1
+            if (self.y+1 < 10) and (self.labi[self.y+1][self.x] != '1'):
+                self.labi[self.y][self.x] = ' '
+                self.y = self.y + 1
         elif movimento == 2:
-            self.x = self.x + 1
+            if (self.x+1 < 10) and (self.labi[self.y][self.x+1] != '1'):
+                self.labi[self.y][self.x] = ' '
+                self.x = self.x + 1
         elif movimento == 3:
-            self.x = self.x - 1
+            if (self.x-1 >= 0) and (self.labi[self.y][self.x-1] != '1'):
+                self.labi[self.y][self.x] = ' '
+                self.x = self.x - 1
         elif movimento == 4:
-            self.y = self.y + 1
-            self.x = self.x + 1
+            if (self.y-1 >= 0) and (self.x+1 < 10) and (self.labi[self.y-1][self.x+1] != '1'):
+                self.labi[self.y][self.x] = ' '
+                self.y = self.y - 1
+                self.x = self.x + 1
         elif movimento == 5:
-            self.y = self.y - 1
-            self.x = self.x - 1
+            if (self.y-1 >= 0) and (self.x-1 >= 0) and (self.labi[self.y-1][self.x-1] != '1'):
+                self.labi[self.y][self.x] = ' '
+                self.y = self.y - 1
+                self.x = self.x - 1
         elif movimento == 6:
-            self.y = self.y + 1
-            self.x = self.x + 1
+            if (self.y+1 < 10) and (self.x+1 < 10) and (self.labi[self.y+1][self.x+1] != '1'):
+                self.labi[self.y][self.x] = ' '
+                self.y = self.y + 1
+                self.x = self.x + 1
         elif movimento == 7:
-            self.y = self.y + 1
-            self.x = self.x - 1
+            if (self.y+1 < 10) and (self.x-1 >= 0) and (self.labi[self.y+1][self.x-1] != '1'):
+                self.labi[self.y][self.x] = ' '
+                self.y = self.y + 1
+                self.x = self.x - 1
+        if(self.labi[self.y][self.x] == 'C'):
+            self.comidas -= 1
         self.labi[self.y][self.x] = 'E'
 
     def pode_mover(self, movimento):
@@ -92,6 +113,7 @@ class labiirinto:
                 return True
             return False
 
-labi = labiirinto()
-labi.print_labiirinto()
-print(labi.pode_mover(0))
+    def tem_comida(self):
+        if self.labi[self.y][self.x] == 'C':
+            return True
+        return False
--- main.py
+++ main.py
@@ -1,9 +1,91 @@
+from zipfile import LargeZipFile
 from labirinto import Labirinto
-
+from random import randrange
+import random
 
 def main():
-    labirinto = Labirinto()
-    
+
+    populacao_inical = []
+
+    i = 0
+    for n in range(5):
+        cromossomo = Labirinto()
+        cromossomo_mov = []
+        while i < 100:
+            random = randrange(7)
+            cromossomo.mover(random)
+            cromossomo_mov.append(random)
+            i+=1
+        i = 0
+        # IMPORTANTE A ultima posicao do individuo e a sua aptidao
+        aptidao = calcula_aptidao(cromossomo_mov)
+        cromossomo_mov.append(aptidao)
+        populacao_inical.append(cromossomo_mov)
+
+    melhor_aptidao = 100
+    elitismo_cromo = []
+    for n in range(len(populacao_inical)):
+        aptidao = populacao_inical[i][len(populacao_inical[i])-1]
+        if aptidao == 0:
+            print('Solução encontrada: {}'.format(populacao_inical[i])-1)
+            exit(1)
+        if aptidao < melhor_aptidao:
+            elitismo_cromo = populacao_inical[i]
+            melhor_aptidao = aptidao
+
+    populacao_intermediaria = []
+    populacao_intermediaria.append(elitismo_cromo)
+    for i in range(len(populacao_inical)//2):
+        (pai, mae) = torneio(populacao_inical)
+        corte = randrange(len(pai))
+        filho1, filho2 = crossover(pai[:-1], mae[:-1], corte)
+        aptidao1 = calcula_aptidao(filho1)
+        aptidao2 = calcula_aptidao(filho2)
+        filho1.append(aptidao1)
+        filho2.append(aptidao2)
+        populacao_intermediaria.append(filho1)
+        populacao_intermediaria.append(filho2)
+
+    mutacao(populacao_intermediaria)
+
+    for individuo in populacao_intermediaria:
+        print(individuo)
+
+
+def crossover(a, b, index):
+    return b[:index] + a[index:], a[:index] + b[index:]
+
+def mutacao(populacao):
+    random_ind = randrange(len(populacao))
+    random_index = randrange(len(populacao[random_ind]) - 1)
+    random_gene = randrange(7)
+    populacao[random_ind][random_index] = random_gene
+
+def torneio(populacao):
+    indices = random.sample(range(len(populacao)), 4)
+    pai = []
+    aptidao1 = populacao[indices[0]][len(populacao[indices[0]]) - 1]
+    aptidao2 = populacao[indices[1]][len(populacao[indices[1]]) - 1]
+    if aptidao1 < aptidao2:
+        pai = populacao[indices[0]]
+    else:
+        pai = populacao[indices[1]]
+    mae = []
+    aptidao1 = populacao[indices[2]][len(populacao[indices[2]]) - 1]
+    aptidao2 = populacao[indices[3]][len(populacao[indices[3]]) - 1]
+    if aptidao1 < aptidao2:
+        mae = populacao[indices[2]]
+    else:
+        mae = populacao[indices[3]]
+    return (pai, mae)
+
+
+def calcula_aptidao(seq_movimentos):
+    labi = Labirinto()
+    for movimento in seq_movimentos:
+        labi.mover(movimento)
+    return labi.get_comidas()
+
 
 if __name__ == '__main__':
     main()
