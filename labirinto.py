38fb8aa19726a7e2e2418e8e6bbf6027ead4d9be
Lucas Ramos <lucas.ramos@datacom.ind.br>
2022-09-29 16:02:43 -0300

    ajustes

--- labirinto.py
+++ labirinto.py
@@ -1,26 +1,32 @@
 '''Classe que representa um labiirinto'''
 
+from traceback import print_last
+
+
 class Labirinto:
 
     def __init__(self):
         self.labi = []
-        arquivo = open('labirinto.txt', 'r')
-        i = 1
+        arquivo = open('labirinto1.txt', 'r')
+        self.tamanho_labirinto = int(arquivo.readline())
+        # print('tamanho do labirinto: {}'.format(self.tamanho_labirinto))
         for linha in arquivo:
-            pos = linha.split(',')
-            pos.pop()
+            pos = linha.split(' ')
+            pos[self.tamanho_labirinto-1] = pos[self.tamanho_labirinto-1].strip()
             self.labi.append(pos)
-            i += 1
         self.x = 0
         self.y = 0
-        self.comidas = len(self.labi[0]) // 2
+        self.comidas = self.tamanho_labirinto // 2
+        
 
     def get_comidas(self):
         return self.comidas
 
     def print_labirinto(self):
         for i in range(0,10):
-            print(self.labi[i])
+            for casa in self.labi[i]:
+                print('{}'.format(casa), end=' ')
+            print('')
         print('--------------------------------------------------')
 
     def mover(self, movimento):
@@ -34,38 +40,38 @@ class Labirinto:
             7: down left '''
         if movimento == 0:
             if (self.y-1 >= 0) and (self.labi[self.y-1][self.x] != '1'):
-                self.labi[self.y][self.x] = ' '
+                self.labi[self.y][self.x] = '0'
                 self.y = self.y - 1
         elif movimento == 1:
             if (self.y+1 < 10) and (self.labi[self.y+1][self.x] != '1'):
-                self.labi[self.y][self.x] = ' '
+                self.labi[self.y][self.x] = '0'
                 self.y = self.y + 1
         elif movimento == 2:
             if (self.x+1 < 10) and (self.labi[self.y][self.x+1] != '1'):
-                self.labi[self.y][self.x] = ' '
+                self.labi[self.y][self.x] = '0'
                 self.x = self.x + 1
         elif movimento == 3:
             if (self.x-1 >= 0) and (self.labi[self.y][self.x-1] != '1'):
-                self.labi[self.y][self.x] = ' '
+                self.labi[self.y][self.x] = '0'
                 self.x = self.x - 1
         elif movimento == 4:
             if (self.y-1 >= 0) and (self.x+1 < 10) and (self.labi[self.y-1][self.x+1] != '1'):
-                self.labi[self.y][self.x] = ' '
+                self.labi[self.y][self.x] = '0'
                 self.y = self.y - 1
                 self.x = self.x + 1
         elif movimento == 5:
             if (self.y-1 >= 0) and (self.x-1 >= 0) and (self.labi[self.y-1][self.x-1] != '1'):
-                self.labi[self.y][self.x] = ' '
+                self.labi[self.y][self.x] = '0'
                 self.y = self.y - 1
                 self.x = self.x - 1
         elif movimento == 6:
             if (self.y+1 < 10) and (self.x+1 < 10) and (self.labi[self.y+1][self.x+1] != '1'):
-                self.labi[self.y][self.x] = ' '
+                self.labi[self.y][self.x] = '0'
                 self.y = self.y + 1
                 self.x = self.x + 1
         elif movimento == 7:
             if (self.y+1 < 10) and (self.x-1 >= 0) and (self.labi[self.y+1][self.x-1] != '1'):
-                self.labi[self.y][self.x] = ' '
+                self.labi[self.y][self.x] = '0'
                 self.y = self.y + 1
                 self.x = self.x - 1
         if(self.labi[self.y][self.x] == 'C'):
--- main.py
+++ main.py
@@ -1,42 +1,59 @@
-from zipfile import LargeZipFile
+
+from optparse import OptionParser
 from labirinto import Labirinto
 from random import randrange
 import random
 
 def main():
+    
+    parser = OptionParser(usage='Usage: %prog <-r> <False or True>')
+    parser.add_option('-r', '--exec-rapida', dest='rapido',
+                      default=False, help="Execução rápida")
+    (cfg, _) = parser.parse_args()
 
     populacao_inical = []
     num_geracao = 1
+    TAMANHO_CROMOSSOMO = 100
+    NUMERO_GERACOES = 200
+    CHANCE_MUTACAO = 5      #[0 ou 1 ou 2 ou 3 ou 4]20% de chance 
 
-    i = 0
+    res_log = open('log.txt', 'w')
+
     for n in range(5):
+        i = 0
         cromossomo = Labirinto()
         cromossomo_mov = []
-        while i < 100:
+        while i < TAMANHO_CROMOSSOMO:
             random = randrange(7)
-            cromossomo.mover(random)
-            cromossomo_mov.append(random)
-            i+=1
-        i = 0
+            if cromossomo.pode_mover(random):
+                cromossomo.mover(random)
+                cromossomo_mov.append(random)
+                i+=1
         # IMPORTANTE A ultima posicao do individuo e a sua aptidao
         aptidao = calcula_aptidao(cromossomo_mov)
         cromossomo_mov.append(aptidao)
         populacao_inical.append(cromossomo_mov)
 
-    melhor_aptidao = 100
+    melhor_aptidao = 100000
     geracao = populacao_inical
-    geracao_aux = []
-    num_geracao = 0
 
-    while num_geracao < 200:
+    while num_geracao <= NUMERO_GERACOES:
+
+        for cromossomo in geracao:
+            if cromossomo[len(cromossomo)-1] == 0:
+                print('Solução encontrada:')
+                resposta = Labirinto()
+                for mov in cromossomo:
+                    resposta.mover(mov)
+                    resposta.print_labirinto()
+                exit(1)
+
+        geracao_aux = []
 
         elitismo_cromo = geracao[0]
-        n = 0
-        while n < len(geracao):
-            aptidao = geracao[n][len(geracao[i])-1]
-            if aptidao == 0:
-                print('Solução encontrada: {}'.format(geracao[i])-1)
-                exit(1)
+        for n in range(1, len(geracao)):
+            # print('n: {}'.format(n))
+            aptidao = geracao[n][len(geracao[n])-1]
             if aptidao < melhor_aptidao:
                 elitismo_cromo = geracao[n]
                 melhor_aptidao = aptidao
@@ -44,6 +61,8 @@ def main():
 
 
         geracao_aux.append(elitismo_cromo)
+        # for i in range(len(geracao)//2):
+
         for i in range(len(geracao)//2):
             (pai, mae) = torneio(geracao)
             corte = randrange(len(pai))
@@ -55,29 +74,57 @@ def main():
             geracao_aux.append(filho1)
             geracao_aux.append(filho2)
 
-        ocorre_mutacao = randrange(5)
+        ocorre_mutacao = randrange(CHANCE_MUTACAO)
         if(ocorre_mutacao == 0):
-            mutacao(geracao)
+            mutacao(geracao_aux)
 
         index = 0
-        for individuo in geracao_aux:
-            print('Cromossomo {}: {}'.format(index,individuo))
-            index += 1
-        print('GERAÇÃO: {}'.format(num_geracao))
+        if not cfg.rapido:
+            for individuo in geracao_aux:
+                res_log.write('Cromossomo {}: {}\n'.format(index,individuo))
+                index += 1
+            res_log.write('GERAÇÃO: {}\n'.format(num_geracao))
         
         geracao = geracao_aux
-        geracao_aux = []
         num_geracao += 1
-        
+
+    resposta = Labirinto()
+    for mov in geracao[0]:
+        resposta.mover(mov)
+        resposta.print_labirinto()   
 
 
 def crossover(a, b, index):
-    return b[:index] + a[index:], a[:index] + b[index:]
+    filho1 = b[:index] + a[index:]
+    filho2 = a[:index] + b[index:]
+    labirinto = Labirinto()
+    for i in range(index):
+        labirinto.mover(filho1[i])
+    for j in range(index, len(filho1)):
+        if labirinto.pode_mover(filho1[j]):
+            labirinto.mover(filho1[j])
+        else:
+            while not labirinto.pode_mover(filho1[j]):
+                filho1[j] = randrange(7)
+            labirinto.mover(filho1[j])
+
+    labirinto = Labirinto()
+    for i in range(index):
+        labirinto.mover(filho2[i])
+    for j in range(index, len(filho2)):
+        if labirinto.pode_mover(filho2[j]):
+            labirinto.mover(filho2[j])
+        else:
+            while not labirinto.pode_mover(filho2[j]):
+                filho2[j] = randrange(7)
+            labirinto.mover(filho2[j])
+    return filho1, filho2
 
 def mutacao(populacao):
     random_ind = randrange(len(populacao))
     random_index = randrange(len(populacao[random_ind]) - 1)
     random_gene = randrange(7)
+    print('O cromossomo {} na posicao {} foi alteirado para {}'.format(random_ind, random_index, random_gene))
     populacao[random_ind][random_index] = random_gene
 
 def torneio(populacao):
